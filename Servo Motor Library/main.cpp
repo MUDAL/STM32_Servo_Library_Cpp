761d2933cf17f3b42d965935c5fa28c11b0669d0
Olaoluwa Raji <olaoluwaraji1999@gmail.com>
2021-10-07 04:27:43 +0100

    uploaded code

---
+++ Servo Motor Library/main.cpp
@@ -0,0 +1,29 @@
+#include "servo.h"
+
+int main(void)
+{
+	HAL_Init();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_TIM4_CLK_ENABLE(); 
+	
+	pinStruct_t servoPin = {GPIOB,GPIO_PIN_6};
+	static Servo servo(servoPin,TIM4,GPIO_AF2_TIM4,TIM_CHANNEL_1);
+	
+	while(1)
+	{
+		servo.TurnShaft(SERVO_MIDPOSITION);
+		HAL_Delay(1000);
+		servo.TurnShaft(SERVO_LEFTPOSITION);
+		HAL_Delay(1000);		
+		servo.TurnShaft(SERVO_MIDPOSITION);
+		HAL_Delay(1000);
+		servo.TurnShaft(SERVO_RIGHTPOSITION);
+		HAL_Delay(1000);	
+	}
+}
+
+extern "C" void SysTick_Handler(void)
+{
+	HAL_IncTick();
+}
+
